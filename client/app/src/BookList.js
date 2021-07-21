04db36111670870033b8b9f594b601be47433b5d
adribasn <adrien.basyn@outlook.com>
2021-07-21 13:50:30 +0200

    Create and Read functionality done

--- client/app/src/BookList.js
+++ client/app/src/BookList.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom'
 
 export const BookList = () => {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        axios.get("http://localhost:3001/read").then((response) => {
+            setItems(response.data);
+        })
+    })
+
     return (
         <div className='container'>
             <div className="mt-3">
@@ -17,7 +27,22 @@ export const BookList = () => {
                     </thead>
                     <tbody>
                         {
-                        
+                            items.map((val, key) => {
+                                return (
+                                    <tr key={val._id}>
+                                        <td>{val.title}</td>
+                                        <td>{val.author}</td>
+                                        <td>{val.date}</td>
+                                        <td>{val.rating}</td>
+                                        <td>
+                                            <Link role="button" className="btn btn-primary" to={`/update/${val._id}`}>Edit</Link>
+                                        </td>
+                                        <td>
+                                            <Link role="button" className="btn btn-danger">Delete</Link>
+                                        </td>
+                                    </tr>
+                                )
+                            })
                         }
                     </tbody>
                 </table>
